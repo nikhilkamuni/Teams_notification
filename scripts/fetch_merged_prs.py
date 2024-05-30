df9e22d9a86ab33dbccdf98800074122a3fbe8a8
Kamuni Nikhil <34977685+nikhilkamuni@users.noreply.github.com>
2024-05-30 19:11:28 +0530

    latest changes

--- scripts/fetch_merged_prs.py
+++ scripts/fetch_merged_prs.py
@@ -4,14 +4,11 @@ import re
 from github import Github
 from github.Repository import Repository
 
-def get_pull_info(pull_id: int, repo: Repository) -> str:
+def get_pull_details(pull_id: int, repo: Repository) -> (str, str):
     pr = repo.get_pull(pull_id)
-    return f"PR #{pr.number}: {pr.title} by {pr.user.login}"
+    return pr.title, pr.user.login
 
 def check_pr_titles(repo: Repository, src_branch: str, dest_branch: str, regex: str) -> list:
-    # Fetch the latest updates from the remote repository
-    subprocess.check_output(["git", "fetch", "origin"])
-
     gitlog = subprocess.check_output(
         [
             "git",
@@ -22,10 +19,8 @@ def check_pr_titles(repo: Repository, src_branch: str, dest_branch: str, regex: 
         ]
     ).decode()
 
-    print(f"Git log output:\n{gitlog}")  # Debug: Print git log output
-
     title_pattern = re.compile(regex)
-    merge_pattern = re.compile("^Merge pull request #([0-9]+) from .*\$")
+    merge_pattern = re.compile("^Merge pull request #([0-9]+) from .*$")
 
     merged_prs = []
 
@@ -33,12 +28,12 @@ def check_pr_titles(repo: Repository, src_branch: str, dest_branch: str, regex: 
         merge_match = re.search(merge_pattern, line)
         if merge_match:
             pr_id = int(merge_match.group(1))
-            pr_info = get_pull_info(pr_id, repo)
-            merged_prs.append(pr_info)
+            title, user = get_pull_details(pr_id, repo)
+            merged_prs.append(f"PR #{pr_id} by {user}: {title}")
 
     return merged_prs
 
-def main():
+if __name__ == "__main__":
     github_personal_access_token = os.getenv("GITHUB_TOKEN")
     assert github_personal_access_token
 
@@ -48,12 +43,5 @@ def main():
     )
     repo = github_object.get_repo("nikhilkamuni/Teams_notification")
     merged_prs = check_pr_titles(repo, "nightly_success", "main", ".*")
-
-    if not merged_prs:
-        print("No merged PRs found.")
-    else:
-        for pr in merged_prs:
-            print(pr)
-
-if __name__ == "__main__":
-    main()
+    for pr in merged_prs:
+        print(pr)
