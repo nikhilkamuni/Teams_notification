c3b4505c8a214c32e2f9f9fef57532305a2a0d1e
Kamuni Nikhil <34977685+nikhilkamuni@users.noreply.github.com>
2024-05-31 13:31:32 +0530

    Send and hyperlink PRs

--- scripts/fetch_merged_prs.py
+++ scripts/fetch_merged_prs.py
@@ -9,7 +9,7 @@ def get_pull_title(pull_id: int, repo: Repository) -> str:
     pr_url = pr.html_url
     return f"<a href=\"{pr_url}\">PR #{pr.number}</a> {pr.title} by {pr.user.login}"
 
-def check_pr_titles(repo: Repository, src_branch: str, dest_branch: str, regex: str) -> list:
+def check_pr_titles(repo: Repository, src_branch: str, dest_branch: str) -> list:
     gitlog = subprocess.check_output(
         [
             "git",
@@ -20,23 +20,16 @@ def check_pr_titles(repo: Repository, src_branch: str, dest_branch: str, regex: 
         ]
     ).decode()
 
-    # Debug print
-    print("Git log output:\n", gitlog)
-
     merge_pattern = re.compile(r"^Merge pull request #(\d+) from .*")
 
     merged_prs = []
 
-    # Parse each line of the git log output
     for line in gitlog.split("\n"):
-        print("Processing line:", line)  # Debug print
         merge_match = merge_pattern.match(line)
         if merge_match:
             pr_id = int(merge_match.group(1))
             title = get_pull_title(pr_id, repo)
             merged_prs.append(title)
-        else:
-            print("No match found for line:", line)  # Debug print
 
     return merged_prs
 
@@ -47,7 +40,7 @@ def main():
 
     github_object = Github(github_personal_access_token)
     repo = github_object.get_repo("nikhilkamuni/Teams_notification")
-    merged_prs = check_pr_titles(repo, "nightly_success", "main", ".*")
+    merged_prs = check_pr_titles(repo, "nightly_success", "main")
 
     if merged_prs:
         print("\n".join(merged_prs))
